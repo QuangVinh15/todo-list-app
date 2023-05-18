ca633c26e5ffda848c000a520be3fb5d195ed6fb
QuangVinh15 <quangvinh15092002@gmail.com>
2023-05-18 23:24:30 +0700

    finish project

--- todolist/settings.py
+++ todolist/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-+tt@_=e^9il%#c116p!0yge_$pm)a23_gz_7-wdp4$-lagpys5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
+ALLOWED_HOSTS = ['']
 
 
 # Application definition
@@ -122,8 +122,6 @@ LOGIN_URL = 'login'
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
